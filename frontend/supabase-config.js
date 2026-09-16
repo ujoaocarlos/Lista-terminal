2bfbd5ed9ec61b78f037dc70bc1651ab23251554
ujoaocarlos <ujoaocarlos12@gmail.com>
2026-09-16 10:21:29 -0300

    Configura Supabase

--- frontend/supabase-config.js
+++ frontend/supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-    url: "COLE_A_URL_DO_SEU_PROJETO_SUPABASE",
-    anonKey: "COLE_A_CHAVE_ANON_DO_SEU_PROJETO_SUPABASE"
+    url: "https://mwrxjzisqaulqwogtpsf.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13cnhqemlzcWF1bHF3b2d0cHNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1NTgyMDUsImV4cCI6MjEwNTEzNDIwNX0.mse8VoyJi_hWfSBRXpFefLnQjX4WcljbL5XRSLCZx0U"
 };
